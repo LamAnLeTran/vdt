e71226777dc0ac612a72bbfed53923b726cea3cd
An <ghostkey.war@gmail.com>
2021-04-22 19:48:24 +0700

    Database + Register
    add sqlhelpers for db query, form, register backend

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask,render_template, flash, redirect, url_for, session, requ
 from passlib.hash import sha256_crypt
 from flask_mysqldb import MySQL
 
+from sqlhelpers import *
+from forms import *
+# from password import _mysql_password
+
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
@@ -14,9 +18,20 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
+@app.route("/register", methods = ['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    users = Table("user", "name", "email", "username", "password")
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html', form = form)
+    return render_template('register.html', form = form)
+
 @app.route("/")
 def index():
-    return "hello world"
+    # users = Table("user", "name", "email", "username", "password")
+    # users.insert("An1", "ghostkey.war@gmail.com", "red2374", "hash")
+    # users.drop()
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.secret_key = 's123'
